66148f7da6fffde341780b791ee0e1e37ab2b83f
tsaiiii08 <lintsaiting0824@gamil.com>
2023-08-14 20:00:51 +0800

    add message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const usePassport = require('./config/passport')
 const routes = require('./routes')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
@@ -19,9 +20,12 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(express.urlencoded({ extended: true }))
